27b73ede7b9ec89727f292b43c9638e2f7dfffc1
little_dev <littleman11186@gmail.com>
2019-08-19 16:33:40 -0400

    Created api project containing spring boot application and import for angular 6 resources to be provided to the front end.
    Created pom.xml placeholder for ui project

---
+++ README.txt
@@ -0,0 +1,2 @@
+Generated schema_match project from Spring Initializr and https://dzone.com/articles/building-a-web-app-using-spring-boot-angular-6-and
+
---
+++ schema_match_api/src/main/java/org/finra/schemamatch/schema_match_api/SchemaMatchApiApplication.java
@@ -0,0 +1,13 @@
+package org.finra.schemamatch.schema_match_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaMatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaMatchApiApplication.class, args);
+	}
+
+}
